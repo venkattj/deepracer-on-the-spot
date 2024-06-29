0ecc6552c04d9b806ac21fe66ec7a3c2e598b4de
venkattj <37686279+venkattj@users.noreply.github.com>
2024-06-29 16:00:05 +0530

    updataing reward function , hyperparameters.json, model_metadata.json, run.env version 2

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -4,18 +4,15 @@ def reward_function(params):
 
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
-    steering = abs(params['steering_angle'])
-    direction_stearing=params['steering_angle']
+    steering = params['steering_angle']
     speed = params['speed']
-    steps = params['steps']
     progress = params['progress']
-
+    is_reversed = params['is_reversed']
+    is_offtrack = params['is_offtrack']
     all_wheels_on_track = params['all_wheels_on_track']
     ABS_STEERING_THRESHOLD = 15
     SPEED_THRESHOLD = 3
-    TOTAL_NUM_STEPS = 85
-
-
+    DIRECTION_THRESHOLD = 10.0
 
     # Read input variables
     waypoints = params['waypoints']
@@ -23,11 +20,11 @@ def reward_function(params):
     heading = params['heading']
     reward = 1.0
 
-    if(all_wheels_on_track == True):
+    if all_wheels_on_track:
         reward *= 5.0
-
     else:
         reward -= 5.0
+
     if progress == 100:
         reward += 100
 
@@ -40,22 +37,33 @@ def reward_function(params):
     else:
         # High reward if the car stays on track and goes fast
         reward *= 3.0
+    if is_offtrack:
+        reward -= 1e-3
 
     # Calculate the direction of the center line based on the closest waypoints
     next_point = waypoints[closest_waypoints[1]]
     prev_point = waypoints[closest_waypoints[0]]
-    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
     track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
-    # Convert to degree
     track_direction = math.degrees(track_direction)
-    # Calculate the difference between the track direction and the heading direction of the car
     direction_difference = abs(track_direction - heading)
+
     # Penalize the reward if the difference is too large
-    DIRECTION_THRESHOLD = 10.0
-    vid=1
     if direction_difference > DIRECTION_THRESHOLD:
-        vid=1-(direction_difference/50)
-    if vid<0 or vid>1:
-        vid = 0
-        reward *= vid
+        reward *= (1 - (direction_difference / 50))
+
+    # Check if the car is turning left or right
+    if steering > ABS_STEERING_THRESHOLD:
+        # Right turn
+        if distance_from_center < 0:
+            reward += 1.0
+        else:
+            reward -= 1.0
+    elif steering < -ABS_STEERING_THRESHOLD:
+        # Left turn
+        if distance_from_center > 0:
+            reward += 1.0
+        else:
+            reward -= 1.0
+    if is_reversed:
+        reward -= 1e-3
     return float(reward)
