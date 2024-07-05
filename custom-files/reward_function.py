00ca1184ddc77811755fe2ae57cb43248838ed2e
venkattj <37686279+venkattj@users.noreply.github.com>
2024-07-05 23:03:41 +0530

    updataing run.env & reward

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -86,7 +86,7 @@ def reward_function(params):
             center_reward = 0.3
         elif distance_from_center_scaled <= 0.5:
             center_reward = 0.7
-        elif distance_from_center <= 0.8:
+        elif distance_from_center_scaled <= 0.8:
             center_reward = 1
         else:
             center_reward = 0
