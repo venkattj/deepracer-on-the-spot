d51c04f158a3dc640da7d064fb3d349b060d5cf2
venkattj <37686279+venkattj@users.noreply.github.com>
2024-07-07 14:18:36 +0530

    updataing run.env & reward

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -81,7 +81,7 @@ def reward_function(params):
         speed_reward = speed * 1.5
 
     # Adjust reward for bends
-    bend_penalty = 1.75 - distance_from_center_scaled * 1.5
+    bend_penalty = 2 - distance_from_center_scaled
     if direction_diff > ABS_STEERING_THRESHOLD:
         if steering > ABS_STEERING_THRESHOLD:
             steering_penalty = 1
